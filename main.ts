109c62b9b60fc0e41353c45db94df5312bfa5b88
LOBOT-ROBOT <44455318+LOBOT-ROBOT@users.noreply.github.com>
2019-01-28 16:27:30 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -110,6 +110,13 @@ namespace startbit {
         range2 =270
     }
 	
+    export enum startbit_digitaltubePort {
+        //% block="Port 1"
+        port1 = 0x01,
+        //% block="Port 2"
+        port2 = 0x02
+    }
+	
     export enum startbit_CmdType {
         //% block="Invalid command"
         NO_COMMAND = 0,
@@ -204,6 +211,11 @@ namespace startbit {
 
     let macStr: string = "";
     let actiongroup_finished = true;
+    let TM1640_CMD1 = 0x40;
+    let TM1640_CMD2 = 0xC0;
+    let TM1640_CMD3 = 0x80;
+    let _SEGMENTS = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71];
+	
     /**
     * Get the handle command.
     */
@@ -635,6 +647,240 @@ namespace startbit {
     //% weight=87 blockGap=50 blockId=startbit_getBatVoltage block="Get startbit current voltage (mV)"
     export function startbit_getBatVoltage(): number {
         return currentVoltage;
+    }
+	
+/**
+     * TM1640 LED display
+     */
+    export class TM1640LEDs {
+        buf: Buffer;
+        clk: DigitalPin;
+        dio: DigitalPin;
+        _ON: number;
+        brightness: number;
+        count: number;  // number of LEDs
+
+        /**
+         * initial TM1640
+         */
+        init(): void {
+            pins.digitalWritePin(this.clk, 0);
+            pins.digitalWritePin(this.dio, 0);
+            this._ON = 8;
+            this.buf = pins.createBuffer(this.count);
+            this.clear();
+        }
+
+        /**
+         * Start 
+         */
+        _start() {
+            pins.digitalWritePin(this.dio, 0);
+            pins.digitalWritePin(this.clk, 0);
+        }
+
+        /**
+         * Stop
+         */
+        _stop() {
+            pins.digitalWritePin(this.dio, 0);
+            pins.digitalWritePin(this.clk, 1);
+            pins.digitalWritePin(this.dio, 1);
+        }
+
+        /**
+         * send command1
+         */
+        _write_data_cmd() {
+            this._start();
+            this._write_byte(TM1640_CMD1);
+            this._stop();
+        }
+
+        /**
+         * send command3
+         */
+        _write_dsp_ctrl() {
+            this._start();
+            this._write_byte(TM1640_CMD3 | this._ON | this.brightness);
+            this._stop();
+        }
+
+        /**
+         * send a byte to 2-wire interface
+         */
+        _write_byte(b: number) {
+            for (let i = 0; i < 8; i++) {
+                pins.digitalWritePin(this.clk, 0);
+                pins.digitalWritePin(this.dio, (b >> i) & 1);
+                pins.digitalWritePin(this.clk, 1);
+
+            }
+            pins.digitalWritePin(this.clk, 1);
+            pins.digitalWritePin(this.clk, 0);
+        }
+
+        /**
+         * set TM1640 intensity, range is [0-8], 0 is off.
+         * @param val the brightness of the TM1640, eg: 7
+         */
+        //% blockId="TM1640_set_intensity" block="%digitaltube|set intensity %val"
+        //% weight=74 blockGap=8
+        //% parts="TM1640"
+        intensity(val: number = 7) {
+            if (val < 1) {
+                this.off();
+                return;
+            }
+            if (val > 8) val = 8;
+            this._ON = 8;
+            this.brightness = val - 1;
+            this._write_data_cmd();
+            this._write_dsp_ctrl();
+        }
+
+        /**
+         * set data to TM1640, with given bit
+         */
+        _dat(bit: number, dat: number) {
+            this._write_data_cmd();
+            this._start();
+            this._write_byte(TM1640_CMD2 | (bit % this.count))
+            this._write_byte(dat);
+            this._stop();
+            this._write_dsp_ctrl();
+        }
+
+        /**
+         * show a number in given position. 
+         * @param num number will show, eg: 5
+         * @param bit the position of the LED, eg: 0
+         */
+        //% blockId="TM1640_showbit" block="%digitaltube|show digit %num |at %bit"
+        //% weight=76 blockGap=8
+        //% parts="TM1640"
+        showbit(num: number = 5, bit: number = 0) {
+            this.buf[bit % this.count] = _SEGMENTS[num % 16]
+            this._dat(bit, _SEGMENTS[num % 16])
+        }
+
+
+        /**
+          * show a number. 
+          * @param num is a number, eg: 0
+          */
+        //% blockId="TM1640_shownum" block="%digitaltube|show number %num"
+        //% weight=76 blockGap=8
+        //% parts="TM1640"
+        showNumber(num: number) {
+            if (num < 0) {
+                this._dat(0, 0x40) // '-'
+                num = -num
+            }
+            else
+                this.showbit(Math.idiv(num, 1000) % 10)
+            this.showbit(num % 10, 3)
+            this.showbit(Math.idiv(num, 10) % 10, 2)
+            this.showbit(Math.idiv(num, 100) % 10, 1)
+        }
+
+        /**
+          * show a hex number. 
+          * @param num is a hex number, eg: 0
+          */
+        //% blockId="TM1640_showhex" block="%digitaltube|show hex number %num"
+        //% weight=76 blockGap=8
+        //% parts="TM1640"
+        showHex(num: number) {
+            if (num < 0) {
+                this._dat(0, 0x40) // '-'
+                num = -num
+            }
+            else
+                this.showbit((num >> 12) % 16)
+            this.showbit(num % 16, 3)
+            this.showbit((num >> 4) % 16, 2)
+            this.showbit((num >> 8) % 16, 1)
+        }
+
+        /**
+         * show or hide dot point. 
+         * @param bit is the position, eg: 1
+         * @param show is show/hide dp, eg: true
+         */
+        //% blockId="TM1640_showDP" block="%digitaltube|DotPoint at %bit|show %show"
+        //% weight=76 blockGap=8
+        //% parts="TM1640"
+        showDP(bit: number = 1, show: boolean = true) {
+            bit = bit % this.count
+            if (show) this._dat(bit, this.buf[bit] | 0x80)
+            else this._dat(bit, this.buf[bit] & 0x7F)
+        }
+
+        /**
+         * clear LED. 
+         */
+        //% blockId="TM1640_clear" block="clear %digitaltube"
+        //% weight=75 blockGap=8
+        //% parts="TM1640"
+        clear() {
+            for (let i = 0; i < this.count; i++) {
+                this._dat(i, 0)
+                this.buf[i] = 0
+            }
+        }
+
+        /**
+         * turn on LED. 
+         */
+        //% blockId="TM1640_on" block="turn on %digitaltube"
+        //% weight=75 blockGap=8
+        //% parts="TM1640"
+        on() {
+            this._ON = 8;
+            this._write_data_cmd();
+            this._write_dsp_ctrl();
+        }
+
+        /**
+         * turn off LED. 
+         */
+        //% blockId="TM1640_off" block="turn off %digitaltube"
+        //% weight=75 blockGap=8
+        //% parts="TM1640"
+        off() {
+            this._ON = 0;
+            this._write_data_cmd();
+            this._write_dsp_ctrl();
+        }
+    }
+    /**
+     * 创建 TM1640 对象.
+     * @param clk the CLK pin for TM1640, eg: DigitalPin.P1
+     * @param dio the DIO pin for TM1640, eg: DigitalPin.P2
+     * @param intensity the brightness of the LED, eg: 7
+     * @param count the count of the LED, eg: 4
+     */
+    //% weight=77 blockGap=8
+    //% blockId="TM1640_create" block="%port|intensity %intensity|LED count %count"
+    export function create(port: startbit_digitaltubePort, intensity: number, count: number): TM1640LEDs {
+        let digitaltube = new TM1640LEDs();
+        switch (port) {
+            case startbit_digitaltubePort.port1:
+                digitaltube.clk = DigitalPin.P2;
+                digitaltube.dio = DigitalPin.P1;
+                break;
+            case startbit_digitaltubePort.port2:
+                digitaltube.clk = DigitalPin.P14;
+                digitaltube.dio = DigitalPin.P13;
+                break;
+        }
+
+        if ((count < 1) || (count > 5)) count = 4;
+        digitaltube.count = count;
+        digitaltube.brightness = intensity;
+        digitaltube.init();
+        return digitaltube;
     }
 
     const APDS9960_I2C_ADDR = 0x39;
